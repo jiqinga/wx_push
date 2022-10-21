a6cc755bd04191144ad499a25a25032252436eb1
jiqinga <1758812645@qq.com>
2022-10-21 10:50:36 +0800

    🐛 修复向青龙查询时没有动态传token，而是写死的值

--- 企业微信/src/check_cookie.py
+++ 企业微信/src/check_cookie.py
@@ -9,42 +9,43 @@ from w_cookie import read_jd_cookies
 # 获取token
 url = "https://ql.jiqinga.top/open/auth/token"
 headers = {
-    "accept": "application/json"
+	"accept": "application/json"
 }
 params = {
-    "client_id": "751a9ErD_EbN",
-    "client_secret": "mTJDRZlGOc-jdc9ITkr--i6U"
+	"client_id": "751a9ErD_EbN",
+	"client_secret": "mTJDRZlGOc-jdc9ITkr--i6U"
 }
-headers2 = {
-    "accept": "application/json",
-    'Authorization': 'Bearer 6d6f19e3-4a42-41b3-a966-f53780dc3692',
-}
+
 res = requests.get(url, headers=headers, params=params)
 res = res.json()
 print(res["data"]["token"])
+headers2 = {
+	"accept": "application/json",
+	'Authorization': f'Bearer {res["data"]["token"]}',
+}
 # 获取环境变量
 url = "https://ql.jiqinga.top/open/envs"
 res2 = requests.get(url=url, headers=headers2)
 env_list = res2.json()
 for i in env_list["data"]:
-    if int(i['status']) != 0:
-        try:
-            pt_pin = re.search('pt_pin=.*;', i["value"]).group()
-        except AttributeError:
-            print(i["remarks"], i["value"], "不是京东cookie")
-            continue
-        # print("cookie失效", i["remarks"], pt_pin)
-        cookies = read_jd_cookies()
-        try:
-            for user in cookies:
-                if pt_pin in cookies[user]:
-
-                    qy = qywx(0)
-                    # 发送文本消息
-                    qy.send_text('%s,cookie失效' % pt_pin, [user])
-                else:
-                    print("没有要发送cookie失效的用户")
-                    continue
-        except KeyError:
-            print("%s,暂未找到绑定关系" % pt_pin)
-            continue
+	if int(i['status']) != 0:
+		try:
+			pt_pin = re.search('pt_pin=.*;', i["value"]).group()
+		except AttributeError:
+			print(i["remarks"], i["value"], "不是京东cookie")
+			continue
+		# print("cookie失效", i["remarks"], pt_pin)
+		cookies = read_jd_cookies()
+		try:
+			for user in cookies:
+				if pt_pin in cookies[user]:
+					
+					qy = qywx(0)
+					# 发送文本消息
+					qy.send_text('%s,cookie失效' % pt_pin, [user])
+				else:
+					print("没有要发送cookie失效的用户")
+					continue
+		except KeyError:
+			print("%s,暂未找到绑定关系" % pt_pin)
+			continue
